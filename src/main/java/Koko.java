5939f64cbc4027ebdcd680fafbed86f833a60234
keikoflorensiaang <cakkeoang@gmail.com>
2026-01-15 15:09:45 +0800

    Implement skeletal version that greets and exits

---
+++ src/main/java/Koko.java
@@ -0,0 +1,9 @@
+public class Koko {
+    public static void main(String[] args) {
+        System.out.println(
+                "Hello! I'm Koko\n"
+                + "What can I do for you?\n"
+                + "Bye. Hope to see you again soon!\n"
+        );
+    }
+}
