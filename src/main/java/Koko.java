777fc411a4085d0f1c0fee5dd112c86d4280ca38
keikoflorensiaang <cakkeoang@gmail.com>
2026-01-15 20:47:25 +0800

    Add echo feature to skeletal version

--- src/main/java/Koko.java
+++ src/main/java/Koko.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+
 public class Koko {
+
+    public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println(
                 "Hello! I'm Koko\n"
                 + "What can I do for you?\n"
-                + "Bye. Hope to see you again soon!\n"
         );
+        String input = sc.nextLine();
+        echo(input);
+    }
+
+    private static void echo(String input) {
+        if (input.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!\n");
+        } else {
+            System.out.println(input);
+            echo(sc.nextLine());
+        }
     }
 }
